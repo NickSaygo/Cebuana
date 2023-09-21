0c2688a10cb34686355f5d841a4a4b440f8656d7
NickSaygo <main.saygonick05@gmail.com>
2023-09-21 12:26:35 +0800

    Basic Assets

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// You must have the jquery script in you to make this code running.
+// This script is to call the specific file in inside the file.
+$(function(){
+    $("#navbar-placeholder").load("html/navbar.html");
+});
